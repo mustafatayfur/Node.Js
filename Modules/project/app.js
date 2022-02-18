db86c3e03ad614cd3ad8b604ed16372a84e8c715
mustafa <mltm0204@gmail.com>
2022-02-18 16:07:12 +0300

    fixed

--- Modules/project/app.js
+++ Modules/project/app.js
@@ -7,6 +7,16 @@ const hostname = '127.0.0.1'
 
 app.use(express.static('public'))
 
+app.get('/', (req, res)=> {
+  res.sendFile(path.resolve(__dirname, 'site/index.html'))
+})
+app.get('/about', (req, res)=> {
+  res.sendFile(path.resolve(__dirname, 'site/about.html'))
+})
+app.get('/blog', (req, res)=> {
+  res.sendFile(path.resolve(__dirname, 'site/blog.html'))
+})
+
 
 app.listen(port, hostname,() => {
   console.log(`Server çalısıyor, http://${hostname}:${port}/`)
